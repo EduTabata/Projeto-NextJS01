0e90f71237147c2a3373f3cafa21b8923833e255
EduTabata <dudutabata@gmail.com>
2025-04-22 15:29:56 -0300

    att2

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
 
   async redirects() {
-    return [
-      {
-        source: '/app/Home.tsx',
-        destination: '/',
-        permanent: false,
-      },
-    ];
+    return [];
   },
 };
 
